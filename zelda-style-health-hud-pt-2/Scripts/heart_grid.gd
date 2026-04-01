2159ac1611db377247252986d6e23d664ab0c07b
T. L. Bainter <taran.bainter@gmail.com>
2026-03-31 20:03:52 -0500

    Initial Pt. 2 Commit
    This is the initial commit of Part 2 of the Zelda Style Heart System tutorial series. This commit includes:
    -heart flashing when health is low
    -active heart pulsing
    
    My screenshots and GIFs are also included.

--- zelda-style-health-hud-pt-2/Scripts/heart_grid.gd
+++ zelda-style-health-hud-pt-2/Scripts/heart_grid.gd
@@ -22,9 +22,23 @@ func set_max_hearts(max_hearts : int):
 		add_child(heart)
 
 func update_hearts(cur_health : int):
+	#Heart Pulsing Variables
+	var active_heart_index = int(ceil(cur_health / 4.0)) -1
+	#Core heart variables
 	var hearts = get_children()
+	#Heart Flashing Variables
+	var max_hp : int = int(hearts.size() * 4)
+	var low_hp_threshold : int = clampi(int(floor(max_hp * 0.25)), 4, 15)
+	var hp_low : bool = (cur_health <= low_hp_threshold and cur_health > 0)
+	#Heart filling and state setting
 	for i in range(hearts.size()):
 		var heart = hearts[i] as HeartGUI
 		var heart_fill_value = clampi(cur_health - (i * 4), 0, 4)
 		heart.update(heart_fill_value)
+		#Set pulse
+		var pulse : bool = (i==active_heart_index and heart_fill_value > 0)
+		heart.pulse(pulse)
+		#Set flash
+		var flash : bool = (hp_low and heart_fill_value > 0)
+		heart.flash(flash)
 #endregion FUNCTIONS==============#
--- zelda-style-health-hud-pt-2/Scripts/heart_gui.gd
+++ zelda-style-health-hud-pt-2/Scripts/heart_gui.gd
@@ -6,6 +6,8 @@ extends Panel
 #region VARIABLES==============#
 @export_category("Heart Components")
 @export var sprite : TextureRect
+@export var flash_anim : AnimationPlayer
+@export var pulse_anim : AnimationPlayer
 @export_category("Heart Images")
 ##Full heart
 @export var sprite_4_4_heart : AtlasTexture
@@ -29,4 +31,28 @@ func update(update_value : int):
 		2: sprite.texture = sprite_2_4_heart
 		3: sprite.texture = sprite_3_4_heart
 		4: sprite.texture = sprite_4_4_heart
+
+##Causes the heart to flash
+func flash(flash_state : bool):
+	if not flash_anim:
+		return
+	if flash_state:
+		if flash_anim and flash_anim.current_animation != "heart_flash":
+			flash_anim.play("heart_flash")
+		elif flash_anim.is_playing() or sprite.modulate != Color.WHITE:
+			sprite.modulate = Color.WHITE
+			flash_anim.stop()
+
+##Causes the heart to pulse
+func pulse(pulse_state : bool):
+	if not pulse_anim:
+		return
+	if pulse_state:
+		if pulse_anim and pulse_anim.current_animation != "heart_pulse":
+			pulse_anim.play("heart_pulse")
+	elif pulse_anim.is_playing or sprite.scale != Vector2.ONE:
+		sprite.scale = Vector2.ONE
+		pulse_anim.play("RESET")
+	
+
 #endregion FUNCTIONS==============#
